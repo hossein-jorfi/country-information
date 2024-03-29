6d085c0dec3ca9e8e884b22b06df691f85f64481
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-03-29 16:09:35 +0330

    fix bg style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,9 +18,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <BackgroundGradientAnimation>
-        {children}
-
+        <BackgroundGradientAnimation className="w-full">
+          {children}
         </BackgroundGradientAnimation>
       </body>
     </html>
