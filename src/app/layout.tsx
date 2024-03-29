a6f7c964ba6e6cbb83acc8743861700246146968
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-03-29 17:15:39 +0330

    add nedded fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,14 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Open_Sans, Roboto } from "next/font/google";
+import { Vazirmatn } from "next/font/google";
 import "./globals.css";
 import { BackgroundGradientAnimation } from "@/components/ui/background-gradient-animation";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
+const openSans = Open_Sans({
+  subsets: ["latin"],
+});
+export const vazir = Vazirmatn({ subsets: ["arabic"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${openSans.className}`}>
         <BackgroundGradientAnimation className="w-full">
           {children}
         </BackgroundGradientAnimation>
