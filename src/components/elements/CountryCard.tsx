58cf0366f2ac5510e844d6453bb5026d6b2b1d97
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-03-29 16:53:26 +0330

    add country card items

--- src/components/elements/CountryCard.tsx
+++ src/components/elements/CountryCard.tsx
@@ -1,28 +1,47 @@
+import Image from "next/image";
+
 type CountryCardProps = {
+  region: string;
   name: {
     common: string;
     official: string;
-    region: string;
-    flags: {
-      png: string;
-      svg: string;
-      alt: string;
+    native: {
+      common: string;
+      official: string;
     };
-    //     native: {
-    //       common: string;
-    //       official: string;
-    //     };
   };
+  flags: {
+    png: string;
+    svg: string;
+    alt: string;
+  };
+  flag: string;
 };
 
-const CountryCard = ({ name }: CountryCardProps) => {
+const CountryCard = ({ name, flag, flags, region }: CountryCardProps) => {
   return (
     <div
       className="p-5 w-80 h-44 !border-opacity-50 rounded-lg 
-    bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-100
-    "
+    bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-100 text-gray-300"
     >
-      <div>card</div>
+      <div className="flex items-center text-2xl">
+        <p>{flag}</p>
+        <p className="ml-1">{name?.common}</p>
+      </div>
+
+      <div>
+        <p>{name?.official}</p>
+        <p>{region}</p>
+      </div>
+
+      <div dir="auto">
+        <p>{name?.native?.common}</p>
+        <p>{name?.native?.official}</p>
+      </div>
+
+      {/* <div>
+        <Image width={32} height={20} src={flags?.svg} alt={flags?.alt} />
+      </div> */}
     </div>
   );
 };
--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -3,9 +3,23 @@ import CountryCard from "../elements/CountryCard";
 const HomePage = () => {
   return (
     <div className="flex justify-between w-full">
-      <CountryCard />
-      <CountryCard />
-      <CountryCard />
+      <CountryCard
+        flags={{
+          png: "https://flagcdn.com/w320/ir.png",
+          svg: "https://flagcdn.com/ir.svg",
+          alt: "The flag of Iran is composed of three equal horizontal bands of green, white and red. A red emblem of Iran is centered in the white band and Arabic inscriptions in white span the bottom edge of the green band and the top edge of the red band.",
+        }}
+        name={{
+          common: "Iran",
+          official: "Islamic Republic of Iran",
+          native: {
+            official: "جمهوری اسلامی ایران",
+            common: "ایران",
+          },
+        }}
+        region="Asia"
+        flag="🇮🇷"
+      />
     </div>
   );
 };
