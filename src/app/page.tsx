c099256045c39d620c0f404a49d7489ca1e758e3
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-04-03 12:25:25 +0330

    revalidate data every 24 hour

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 import HomePage from "@/components/pages/HomePage";
 
 async function getData() {
-  const res = await fetch("https://restcountries.com/v3.1/all");
+  const revalidate = 3600 * 24; // update data every 24 hour
+  const res = await fetch("https://restcountries.com/v3.1/all", {
+    next: { revalidate },
+  });
 
   if (!res.ok) {
     throw new Error("Failed to fetch data");
