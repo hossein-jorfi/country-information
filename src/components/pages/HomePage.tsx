7be885111ca541925a6b12db5713c57b6b088bbe
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-04-03 16:07:03 +0330

    show dynamic data in country card

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -7,28 +7,52 @@ type HomePageProps = {
 };
 
 const HomePage = ({ data }: HomePageProps) => {
-  console.log(data);
+  console.log(
+    data[0].name.nativeName[Object.keys(data[0].name?.nativeName)[0]]
+  );
   return (
     <div className="grid grid-cols-3 gap-2 w-full">
-      {data?.map((item: CountryType, index) => {
+      {data?.map((item: any, index) => {
+        if (item?.name?.nativeName) {
+          console.log(
+            item?.name?.nativeName[Object?.keys(item.name.nativeName)[0]]
+              .official
+          );
+        }
         return (
           <CountryCard
             key={item?.name?.common || index}
-            flags={{
-              png: "https://flagcdn.com/w320/ir.png",
-              svg: "https://flagcdn.com/ir.svg",
-              alt: "The flag of Iran is composed of three equal horizontal bands of green, white and red. A red emblem of Iran is centered in the white band and Arabic inscriptions in white span the bottom edge of the green band and the top edge of the red band.",
-            }}
+            // flags={{
+            //   png: "https://flagcdn.com/w320/ir.png",
+            //   svg: "https://flagcdn.com/ir.svg",
+            //   alt: "The flag of Iran is composed of three equal horizontal bands of green, white and red. A red emblem of Iran is centered in the white band and Arabic inscriptions in white span the bottom edge of the green band and the top edge of the red band.",
+            // }}
+            // name={{
+            //   common: "Iran",
+            //   official: "Islamic Republic of Iran",
+            //   native: {
+            //     official: "جمهوری اسلامی ایران",
+            //     common: "ایران",
+            //   },
+            // }}
+            // region="Asia"
+            // flag="🇮🇷"
+            {...item}
             name={{
-              common: "Iran",
-              official: "Islamic Republic of Iran",
+              ...item.name,
               native: {
-                official: "جمهوری اسلامی ایران",
-                common: "ایران",
+                official: item?.name?.nativeName
+                  ? item?.name?.nativeName[
+                      Object?.keys(item?.name?.nativeName || {})[0]
+                    ].official
+                  : "-",
+                common: item?.name?.nativeName
+                  ? item?.name?.nativeName[
+                      Object?.keys(item?.name?.nativeName || {})[0]
+                    ].common
+                  : "-",
               },
             }}
-            region="Asia"
-            flag="🇮🇷"
           />
         );
       })}
