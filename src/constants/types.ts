f009fa134b1aa9f0924a02fcf52b63d1b4f430a0
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-04-03 12:16:15 +0330

    create country type const

---
+++ src/constants/types.ts
@@ -0,0 +1,17 @@
+export type CountryType = {
+  region: string;
+  name: {
+    common: string;
+    official: string;
+    native: {
+      common: string;
+      official: string;
+    };
+  };
+  flags: {
+    png: string;
+    svg: string;
+    alt: string;
+  };
+  flag: string;
+};
