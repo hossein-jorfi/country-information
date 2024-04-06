a5e9e4bdbd330fb15b37225d8b260c7e913ca156
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-04-06 15:18:22 +0330

    map throw regions mock

--- src/components/elements/filters/Region.tsx
+++ src/components/elements/filters/Region.tsx
@@ -1,5 +1,43 @@
+"use client";
+
+import { useState } from "react";
+
+const regionsMock = [
+  "Europe",
+  "Africa",
+  "Americas",
+  "Oceania",
+  "Asia",
+  "Antarctic",
+];
+
 const Region = () => {
-  return <div>Region</div>;
+  const [isHover, setIsHover] = useState(false);
+  return (
+    <div className="mt-4">
+      <div
+        onMouseEnter={() => setIsHover(true)}
+        onMouseLeave={() => setIsHover(false)}
+        className="
+          p-3 w-fit cursor-default
+          bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 text-gray-300
+          rounded-md
+          
+          "
+      >
+        Region
+      </div>
+      {isHover && (
+        <div onMouseEnter={() => setIsHover(true)}>
+          <div onMouseLeave={() => setIsHover(false)} className="border w-fit">
+            {regionsMock.map((item, index) => (
+              <div key={index}>{item}</div>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Region;
