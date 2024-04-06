8bf22fcc234738460fd1ae4b60d3b4167c2598d9
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-04-06 14:52:26 +0330

    search input style

--- src/components/elements/filters/Search.tsx
+++ src/components/elements/filters/Search.tsx
@@ -3,13 +3,14 @@ const Search = () => {
     <div>
       <input
         type="text"
-        placeholder="search..."
+        placeholder="Search"
         className="
         w-full h-10 p-5
         bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 text-gray-300
         rounded-md
         focus:outline-none
-        placeholder:text-gray-400
+        placeholder:text-gray-300
+        placeholder:text-opacity-50
         "
       />
     </div>
