7006d47757a5b39bd23e51110d58ad6db2479af0
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-04-06 14:24:41 +0330

    create filter components

---
+++ src/components/modules/Filter.tsx
@@ -0,0 +1,13 @@
+import Region from "../elements/filters/Region";
+import Search from "../elements/filters/Search";
+
+const Filter = () => {
+  return (
+    <>
+      <Search />
+      <Region />
+    </>
+  );
+};
+
+export default Filter;
