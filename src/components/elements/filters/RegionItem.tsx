cecfa8b4f1141eb09b9f10fc2c163758e45ed8f0
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-04-06 16:08:06 +0330

    style

--- src/components/elements/filters/RegionItem.tsx
+++ src/components/elements/filters/RegionItem.tsx
@@ -1,6 +1,17 @@
-const RegionItem = ({ region, onClick }: { region: string, onClick: () => void }) => {
+const RegionItem = ({
+  region,
+  className,
+  onClick,
+}: {
+  region: string;
+  className?: string;
+  onClick: () => void;
+}) => {
   return (
-    <div onClick={onClick} className="py-1 px-2 bg-gray-400 bg-opacity-0 text-gray-300 hover:bg-opacity-20 rounded-md cursor-pointer">
+    <div
+      onClick={onClick}
+      className={`${className} py-1 px-2 bg-gray-400 bg-opacity-0 text-gray-300 hover:bg-opacity-20 rounded-md cursor-pointer`}
+    >
       {region}
     </div>
   );
