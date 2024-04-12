051d50225c6ba981d55303ae6c6978aac09d8145
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-04-12 11:26:58 +0330

    region filter style

--- src/components/elements/filters/Region.tsx
+++ src/components/elements/filters/Region.tsx
@@ -40,7 +40,7 @@ const Region = () => {
         onClick={() => setIsHover(!isHover)}
         onMouseLeave={() => setIsHover(false)}
         className="
-          p-2 w-fit cursor-default
+          p-1 w-fit cursor-default
           bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 text-gray-300
           rounded-md
           flex flex-wrap
